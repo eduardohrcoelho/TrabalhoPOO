7a1964cf29e8e33b4c98ea34d6bef5476f84b679
Eduardo Coelho <eduardohrcoelhomkt@gmail.com>
2025-11-07 15:32:44 -0300

    Implementando a classe Tesouro com atributos e metodos

--- CaçaTesouro/src/Tesouro.java
+++ CaçaTesouro/src/Tesouro.java
@@ -1,3 +1,28 @@
 public class Tesouro {
+    private String cor;
+    private double pontos;
+
+    public Tesouro(String cor){
+        this.cor = cor;
+        
+        if(this.cor.equalsIgnoreCase("amarelo")){
+            this.pontos = 4.0;
+        }else if(this.cor.equalsIgnoreCase("verde")){
+            this.pontos = 6.0;
+        }else if(this.cor.equalsIgnoreCase("vermelho")){
+            this.pontos = 10.0;
+        }else{
+            System.out.println("Erro! Cor inválida.");
+            this.pontos = 0.0;
+        }
+    }
+
+    public String getCor(){
+        return this.cor;
+    }
+
+    public double getPontos(){
+        return this.pontos;
+    }
 
 }
