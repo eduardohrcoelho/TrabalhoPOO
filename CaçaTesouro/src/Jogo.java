8f38d1a35b6a0fedc291d3e313d9dabb988542a5
Mateus Von <mateusvon205@gmail.com>
2025-11-15 15:49:04 -0300

    Alterando uma palavra

--- CaçaTesouro/src/Jogo.java
+++ CaçaTesouro/src/Jogo.java
@@ -212,7 +212,8 @@ public class Jogo {
       if (tesouroAchado != null) {
         double pontosGanhos = tesouroAchado.getPontos();
         atacante.registrarResultadoDoAtaque(linha, coluna, pontosGanhos); // Exibe 'X' no tabuleiro de ataque
-        System.out.println("Acertou um tesouro da cor " + tesouroAchado.getCor() + "! Ganhou " + pontosGanhos + " pontos!");
+        System.out
+            .println("Achou um tesouro da cor " + tesouroAchado.getCor() + "! Ganhou " + pontosGanhos + " pontos!");
         atacante.adicionarPontos(pontosGanhos);
       } else {
         atacante.registrarResultadoDoAtaque(linha, coluna, 0); // Exibe 'O' no tabuleiro de ataque
